1547f89839b8d213d41f1ad5f4dc9fb961e62a6c
ovass <vassarottowen@gmail.com>
2023-04-16 23:07:38 -0300

    Finished startTurn function and modified the paragraph win-pint to you-won-lost

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-let player = 0;
+let playerScore = 0;
 let computerScore = 0;
 
 let instructions = document.querySelector("#instructions");
 let playerScoreMarker = document.querySelector("#player-score");
 let computerScoreMarker = document.querySelector("#computer-score");
 let message = document.querySelector("#message");
-let winPoint = document.querySelector("#win-point")
+let youWonLost = document.querySelector("#you-won-lost");
 
 let playerChoice = document.querySelector("#player-choice");
 let computerChoice = document.querySelector("#computer-choice");
 
 let options = document.querySelectorAll(".option");
-options.forEach(button =>{
-    button.addEventListener("click",startTurn);
+options.forEach((button) => {
+  button.addEventListener("click", startTurn);
 });
 
-function startTurn(e){
-    let computerOption = Math.floor(Math.random() * 3 + 1);
-    console.log(computerOption);
+function startTurn(e) {
+  let computerOption = Math.floor(Math.random() * 3 + 1);
+  let playerOption = e.currentTarget.id;
+  // console.log("Player choice: " + playerOption);
+
+  if (computerOption === 1) {
+    computerOption = "rock🪨";
+  } else if (computerOption === 2) {
+    computerOption = "paper🧻";
+  } else if (computerOption === 3) {
+    computerOption = "scissors✂️";
+  }
+  // console.log("Computer choice: " + computerOption);
+
+  /* 
+    Si un jugador elige piedra y el otro jugador elige tijera, la piedra gana.
+    Si un jugador elige tijera y el otro jugador elige papel, las tijeras ganan.
+    Si un jugador elige papel y el otro jugador elige piedra, el papel gana.
+    */
+
+    if(
+        (playerOption === "rock🪨" && computerOption === "scissors✂️") ||
+        (playerOption === "paper🧻" && computerOption === "rock🪨") ||
+        (playerOption === "scissors✂️" && computerOption === "paper🧻")
+    ){
+        playerWins();
+    }else if(
+        (computerOption === "rock🪨" && playerOption === "scissors✂️") ||
+        (computerOption === "paper🧻" && playerOption === "rock🪨") ||
+        (computerOption === "scissors✂️" && playerOption === "paper🧻")
+    ){
+        computerWins();
+    }
+    else{
+        tie();
+    }
+}
+
+function playerWins(){
+    
 }
